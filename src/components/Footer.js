f5bd7a270d352b0cdbbedf143a223e5922e0cd2c
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-07-29 13:45:17 +0300

    added content to the footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { FaFacebookSquare } from "react-icons/fa"
+import { FaTwitterSquare } from "react-icons/fa"
+import { FaInstagramSquare } from "react-icons/fa"
+import {FaLinkedin} from "react-icons/fa"
 import "./Footer.css"
 
 function Footer() {
@@ -6,7 +10,7 @@ function Footer() {
     <div className='footer-section'>      
       <div className='inner_section'>
         <div className='higher section'>
-          <h4>Quick Links</h4>
+          <h4></h4>
           <ul>
             <li>P O Box</li>
             <li>154325-34 Nairobi, Kenya</li>
@@ -18,7 +22,7 @@ function Footer() {
           <ul>
             <li>Tembea Kenya</li>
             <li>Email: safaritours@live.com</li>
-            <li>Phone : =254020287872</li>
+            <li>Phone : +254020287872</li>
             <li>Office Location: Nairobi Kenya</li>
             <li>Kimathi Street, Oppoite Kimathi Junction, 5th floor</li>
           </ul>
@@ -26,27 +30,34 @@ function Footer() {
         <div className='higher section'>
           <h4>Other services</h4>
           <ul>
-            <li>I will type here</li>
-            <li>I will type here</li>
-            <li>I will type here</li>
-            <li>I will type here</li>
-            <li>I will type here</li>
+            <li>Tour car hiring</li>
+            <li>Tour giding</li>
+            <li>Pet dog hiring</li>
+            
           </ul>
         </div>
-        <div className='higher section'>
-          <h3>Social handle</h3>
-          <ul>
-            <li>I will type here</li>
-            <li>I will type here</li>
-            <li>I will type here</li>
-            <li>I will type here</li>
-            <li>I will type here</li>
+        <div>
+          <ul className='social'>
+            
+            <li>
+              <a href='https://www.facebook.com/'><FaFacebookSquare /></a>
+            </li>
+            <li>
+              <a href='https://twitter.com/'><FaTwitterSquare /></a>
+            </li>
+            <li>
+              <a href='https://www.instagram.com/'><FaInstagramSquare /></a>
+            </li>
+            <li>
+            <a href='linkedin.com'><FaLinkedin /></a>
+            </li>
+            
           </ul>
         </div>
       </div>
       
       <div className='lower_section'>        
-        
+        <p>Copyright © 2022 tembea kenya by Earnest Achayo. All rights reserved. | Privacy Policy | Terms of use</p>
       </div>
     </div>
   )
